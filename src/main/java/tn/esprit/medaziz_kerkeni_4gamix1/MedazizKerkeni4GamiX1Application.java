dc93fd17900fc62b3a39496ade9387fe5040b30e
AzizKerkeni24 <aziz.kerkeni@esprit.tn>
2025-10-09 22:20:49 +0100

    first commit

---
+++ src/main/java/tn/esprit/medaziz_kerkeni_4gamix1/MedazizKerkeni4GamiX1Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.medaziz_kerkeni_4gamix1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedazizKerkeni4GamiX1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedazizKerkeni4GamiX1Application.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/medaziz_kerkeni_4gamix1/MedazizKerkeni4GamiX1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.medaziz_kerkeni_4gamix1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedazizKerkeni4GamiX1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
